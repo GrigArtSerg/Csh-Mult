f6061e91c429bfa2205ed2f1e9ba18ab7be261c7
GrigArtSerg <56555610+GrigArtSerg@users.noreply.github.com>
2022-09-27 20:11:57 +0300

    Upload

---
+++ MTTest1/Program.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+
+namespace MTTest1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Команда Get возвращает значение Count");
+            Console.WriteLine("Rоманда Add N добавляет число N к значению Count");
+
+            int n = 0;
+
+            while (true)
+            {
+                string[] input = Console.ReadLine().Split(" ");
+                if (input[0].ToLower() == "add")
+                {
+                    if (input.Length == 2) Server.AddToCount(Convert.ToInt32(input[1]));
+                    else Console.WriteLine("Неверная команда");
+                }
+                else if (input[0].ToLower() == "get")
+                {
+                    Console.WriteLine(Server.GetCount());
+                    n++;
+                }
+                else Console.WriteLine("Неверная команда");
+            }
+        }
+    }
+
+
+    /// <summary>
+    /// "Сервер" чистый
+    /// </summary>
+    static class Server
+    {
+        static int Count = 0;
+        static ReaderWriterLockSlim Lock = new ReaderWriterLockSlim();
+
+        /// <summary>
+        /// Чтение переменной Count
+        /// </summary>
+        /// <returns></returns>
+        public static int GetCount()
+        {
+            Task<int> Read = new Task<int>(() =>
+            {
+                Lock.EnterReadLock();
+                try
+                {
+                    return Count;
+                }
+                finally
+                {
+                    Lock.ExitReadLock();
+                }
+            });
+            Read.Start();
+            return Count;
+        }
+
+        /// <summary>
+        /// Добавление значения к Count
+        /// </summary>
+        /// <param name="C"></param>
+        public static void AddToCount(int C)
+        {
+            Thread AddCall = new Thread(obj =>
+            {
+                Lock.EnterWriteLock();
+                try
+                {
+                    if (obj is int C)
+                    {
+                        Count += C;
+                    }
+                }
+                finally
+                {
+                    Lock.ExitWriteLock();
+                }
+
+            });
+            AddCall.Start(C);
+        }
+    }
+
+    #region Альтернативный вариант
+    /*
+    /// <summary>
+    /// "Сервер"
+    /// </summary>
+    static class Server
+    {
+        static int Count = 0;
+        static Semaphore Writer = new Semaphore(1, 1);
+        
+        static bool IsWriting = false;
+
+        /// <summary>
+        /// Серверная функция для чтения переменной Count
+        /// </summary>
+        /// <param name="n">переменная для хранения номера вызова (для отладки)</param>
+        /// <returns></returns>
+        public static int GetCount(int n)
+        {
+            Task<int> Read = new Task<int>(() =>
+            {
+                while (IsWriting)
+                {
+                    Thread.Sleep(100);
+                }
+                Console.WriteLine($"Поток {n}");
+                for (int i = 0; i < 10; i++)
+                {
+                    //Console.WriteLine($"Читаааем");
+                    Thread.Sleep(500);
+                }
+                Console.WriteLine($"вывод: {Count} - Поток {n}");
+
+                return Count;
+            });
+            Read.Start();
+            return Count;
+        }
+
+        /// <summary>
+        /// Серверная функция добавления значения к Count
+        /// </summary>
+        /// <param name="C"></param>
+        public static void AddToCount(int C)
+        {
+            Thread AddCall = new Thread(obj =>
+            {
+                if (obj is int C)
+                {
+                    IsWriting = true;
+                    Writer.WaitOne();
+
+                    Console.WriteLine($"Поток прибавления");
+                    Thread.Sleep(1000);
+
+                    for (int i = 0; i < 10; i++)
+                    {
+                        Console.WriteLine($"Прибавляяяяем");
+                        Thread.Sleep(500);
+                    }
+
+                    Count += C;
+
+                    Console.WriteLine($"Прибавление успено завершено");
+                    Thread.Sleep(1000);
+
+                    Console.WriteLine($"Now Count = {Count}");
+
+                    IsWriting = false;
+                    Writer.Release();
+                }
+            });
+
+            AddCall.Start(C);
+            
+        }
+    }*/
+
+    #endregion
+}
---
+++ MTTest2/Program.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MTTest2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Начало работы программы");
+            EventHandler h = new EventHandler(myEventHandler); // адрес метода
+            
+            // Синхронно
+            h.Invoke(null, EventArgs.Empty); // вызов делегата
+
+            // Асинхронно
+            var res = h.BeginInvoke(null, EventArgs.Empty, null, null);
+
+            // Полусинхронно
+            AsyncCaller ac = new AsyncCaller(h);
+            bool completedOK = ac.Invoke(5000, null, EventArgs.Empty);
+            
+            Console.WriteLine("Конец программы");
+        }
+
+        //async static Task<int> Test(int n) { }
+
+        static void myEventHandler(object sender, EventArgs e)
+        {
+            Console.WriteLine("Делегат выполняет метод");
+        }
+    }
+
+    /// <summary>
+    /// "Полусинхронного" в данном случае означает, что делегат будет вызван,
+    /// и вызывающий поток будет ждать, пока вызов не выполнится.
+    /// Но если выполнение делегата займет больше 5000 миллисекунд, то
+    /// ac.Invoke выйдет и вернет в completedOK значение false.
+    /// </summary>
+    class AsyncCaller
+    {
+        static EventHandler Del;
+        static bool IsCompleted;
+
+        public AsyncCaller(EventHandler EventHand)
+        {
+            Del = EventHand;
+        }
+
+        /// <summary>
+        /// Внешняя функция для вызова делегата с ограничением на время выполнения
+        /// </summary>
+        /// <param name="Timer">ограничение по времени на выполнение</param>
+        /// <param name="obj"></param>
+        /// <param name="EventArg"></param>
+        /// <returns>Успел ли выполниться делегат</returns>
+        internal bool Invoke(int Timer, object? obj, System.EventArgs EventArg)
+        {
+            return HalfAsync(Timer, obj, EventArg).Result;
+        }
+
+        /// <summary>
+        /// Запуск двух параллельных потоков. Кто раньше - такой и bool
+        /// </summary>
+        /// <param name="Timer">ограничение по времени на выполнение</param>
+        /// <param name="obj"></param>
+        /// <param name="EventArg"></param>
+        /// <returns>False - завершился раньше таймер, True - завершился раньше делегат</returns>
+        async static Task<bool> HalfAsync(int Timer, object? obj, System.EventArgs EventArg)
+        {
+            var TimerTask = AsyncCaller.Timer(Timer);
+            var Event = AsyncCaller.Event(obj, EventArg);
+
+            IsCompleted = await Task.WhenAny(TimerTask, Event).Result; // кто быстрее - тот и прав
+            Console.WriteLine($"Первым завершилось {IsCompleted}");
+            return IsCompleted;
+        }
+
+        /// <summary>
+        /// Таймер, ограничивающий время выполнения делегата
+        /// </summary>
+        /// <param name="Time"></param>
+        /// <returns></returns>
+        async static Task<bool> Timer(int Time)
+        {
+            Console.WriteLine("Запуск таймера");
+            await Task.Delay(Time);
+            Console.WriteLine("Конец таймера");
+            return false;
+        }
+
+        /// <summary>
+        /// Делегат
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <param name="EventArg"></param>
+        /// <returns></returns>
+        async static Task<bool> Event(object? obj, System.EventArgs EventArg)
+        {
+            Console.WriteLine("Запуск делегата");
+            await Task.Delay(4000);
+            Del.BeginInvoke(obj, EventArg, null, null);
+            return true;
+        }
+    }
+}
